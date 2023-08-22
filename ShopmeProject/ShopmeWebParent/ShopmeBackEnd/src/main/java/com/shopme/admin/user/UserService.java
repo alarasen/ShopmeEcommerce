75af1151151cf5fc38d4fc046018788607db8805
Berkay Baris Algun <bbarisalgun@gmail.com>
2023-08-22 19:22:48 +0300

    added users in order to test

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -18,7 +18,7 @@ import java.util.NoSuchElementException;
 public class UserService {
 
 
-    public static final int USERS_PER_PAGE=4;
+    public static final int USERS_PER_PAGE=20;
 
 
     @Autowired
